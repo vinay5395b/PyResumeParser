a9ff3d8021afec95fbac669f6fdc696f63b9579c
vinay5395b <vinay5395b@gmail.com>
2019-11-10 16:36:52 -0600

    small changes

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def upload():
         #params = urllib.parse.urlencode(data)
         url="https://webfolio-hackathon.herokuapp.com/fetch_data/"+user_id
         requests.post(url, data = data)
-        return "ok"
+        return redirect("https://webfolio-hackathon.herokuapp.com/show")
         #return render_template('text.html', data=data) #res_to_dict(file.filename)
 
 def res_to_dict(filename):
